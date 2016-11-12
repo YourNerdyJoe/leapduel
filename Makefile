5d1ae4842384956ecd1b47e5ee88c8368969ffca
Joseph LoManto <joe@debian>
2016-11-12 17:15:38 -0500

    test libs and makfile

---
+++ Makefile
@@ -0,0 +1,33 @@
+
+SOURCES := client common
+BUILD := client/build
+CPP_FILES := $(foreach dir, $(SOURCES), $(notdir $(wildcard $(dir)/*.cpp)))
+O_FILES := $(addprefix $(BUILD)/, $(CPP_FILES:.cpp=.o))
+DEPENDS := $(O_FILES:.o=.d)
+
+CC := g++
+CC_FLAGS := -g -Wall -O2 -std=c++11 -I /usr/include/SDL2/
+LD_FLAGS := -lSDL2 -lSDL2_image -lSDL2_net
+
+TARGET := leapduel
+
+all: $(TARGET) $(BUILD)
+
+$(TARGET) : $(O_FILES)
+	$(CC) $(LD_FLAGS) -o $@ $^
+
+$(BUILD)/%.o : client/%.cpp $(BUILD)
+	$(CC) $(CC_FLAGS) -c -o $@ $<
+
+$(BUILD)/%.o : common/%.cpp $(BUILD)
+	$(CC) $(CC_FLAGS) -c -o $@ $<
+
+$(BUILD):
+	mkdir -p $(BUILD)
+
+clean: 
+	rm $(BUILD)/*.o $(BUILD)/*.d $(TARGET)
+
+CC_FLAGS += -MMD
+-include $(DEPENDS)
+
---
+++ client/client_main.cpp
@@ -0,0 +1,74 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+int main(int argc, char* argv[])
+{
+	SDL_Window* main_window;
+	SDL_Renderer* main_renderer;
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	main_window = SDL_CreateWindow(	"title", 
+									SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+									SCREEN_WIDTH, SCREEN_HEIGHT,
+									SDL_WINDOW_SHOWN );
+
+	if(!main_window)
+	{
+		printf("error creating window: %s\n", SDL_GetError());
+		return false;
+	}
+
+	main_renderer = SDL_CreateRenderer(main_window, -1, SDL_RENDERER_ACCELERATED /*| SDL_RENDERER_PRESENTVSYNC*/);
+	if(!main_renderer)
+	{
+		printf("error creating renderer: %s\n", SDL_GetError());
+		return false;
+	}
+
+	//Initialize PNG loading
+    int imgFlags = IMG_INIT_PNG;
+    if( !( IMG_Init( imgFlags ) & imgFlags ) )
+    {
+        printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+        return false;
+    }
+
+	//loop
+
+	SDL_Event ev;
+	bool running = true;
+	while(running)
+	{
+		while(SDL_PollEvent(&ev))
+		{
+			switch(ev.type)
+			{
+				case SDL_QUIT:
+					running = false;
+					break;
+			}
+		}
+		
+		//clear
+		SDL_SetRenderDrawColor(main_renderer, 0, 0, 0, 0);
+		SDL_RenderClear(main_renderer);
+		SDL_SetRenderDrawColor(main_renderer, 255, 255, 255, 255);
+
+		//draw
+
+		//flip
+		SDL_RenderPresent(main_renderer);
+	}
+
+	IMG_Quit();
+
+	if(main_renderer) SDL_DestroyRenderer(main_renderer);
+	if(main_window) SDL_DestroyWindow(main_window);
+	
+	SDL_Quit();
+}
